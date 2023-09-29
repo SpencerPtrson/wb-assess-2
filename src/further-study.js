e8cdf16b81bed36821902139cac49fb9c5ed746e
Spencer Peterson <spencerpeterson.wi@gmail.com>
2023-09-29 13:00:20 -0600

    Did further study question

--- src/further-study.js
+++ src/further-study.js
@@ -31,6 +31,36 @@
 // Ex.:
 //   buildWordChain(['zoo', 'sour', 'racket', 'octos']);
 //   => ['zoo', 'octos', 'sour', 'racket']
-function buildWordChain(words) {}
+function buildWordChain(words) {
+    const resultArr = [words[0]];
+    const wordNum = words.length;
+    words.splice(0, 1);
+  
+  
+    // go until every word has been put into the result arr
+    while (resultArr.length < wordNum) {
+      let foundWord = true;
+      
+      // iterate through the remaining words and check if the first letter is the same as the last letter as the last element in the resultArr
+      for (let i = 0; i < words.length; i++) {
+        let workingWord = resultArr[resultArr.length - 1].split(''); // get array of workingWord to check last character
+        let wordToCheck = words[i].split(''); // get array of wordToCheck to check first character
+        
+        if (workingWord && workingWord.length && wordToCheck && wordToCheck.length && workingWord[workingWord.length - 1] === wordToCheck[0]) {
+          resultArr.push(words[i]);
+          words.splice(i, 1);
+          foundWord = true;
+          break;
+        }
+        else foundWord = false;
+      }
+  
+      if(!foundWord) return resultArr;
+      
+    }
+    console.log(resultArr);
+    return resultArr;
+  }
 
+  
 export { buildWordChain };
